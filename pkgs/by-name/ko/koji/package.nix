18b97c9c55ae1b80b6a749065f81f68f0d510311
Javi Merino <merino.jav@gmail.com>
2024-02-02 17:11:09 +0100

    WIP koji: init at 1.34.0

---
+++ pkgs/by-name/ko/koji/package.nix
@@ -0,0 +1,29 @@
+{
+  lib
+  , fetchzip
+  , python3Packages
+  #, rpm
+}:
+
+python3Packages.buildPythonApplication rec {
+  pname = "koji";
+  version = "1.34.0";
+  src = fetchzip {
+    url = "https://pagure.io/koji/archive/koji-${version}/koji-koji-${version}.tar.gz";
+    hash = "sha256-pUAfg/wS8Wxzj9Udhr/R+QEQvQSdRr8yGFqbDR7pfT0=";
+  };
+  propagatedBuildInputs = with python3Packages; [
+    dateutil
+    requests
+    requests-gssapi
+    six
+  ];
+
+  meta = with lib; {
+    description = "A flexible, secure, and reproducible way to build RPM-based software.";
+    homepage = "https://pagure.io/koji/";
+    maintainers = with maintainers; [ javimerino ];
+    license = licenses.lgpl21Only;
+    platforms = platforms.all;
+  };
+}
