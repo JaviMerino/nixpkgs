238c29dfb99fb0a97469db1965df5a3d5c18f67b
Javi Merino <merino.jav@gmail.com>
2024-02-07 06:19:39 +0000

    python3Packages.requests-gssapi: init at 1.2.3

---
+++ pkgs/development/python-modules/requests-gssapi/default.nix
@@ -0,0 +1,27 @@
+{ fetchPypi
+, lib
+, python3Packages
+}:
+python3Packages.buildPythonPackage rec {
+  pname = "requests-gssapi";
+  version = "1.2.3";
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-IHhFCJgUAfcVPJM+7QlTOJM6QIGNplolnb8tgNzLFQ4=";
+  };
+
+  propagatedBuildInputs = with python3Packages; [
+    gssapi
+    requests
+  ];
+  pythonImportCheck = [ "requests_gssapi" ];
+
+  meta = with lib; {
+    description = "A GSSAPI authentication handler for python-requests";
+    homepage = "https://github.com/pythongssapi/requests-gssapi";
+    maintainers = with maintainers; [ javimerino ];
+    license = [ licenses.isc ];
+    platforms = platforms.all;
+  };
+
+}
